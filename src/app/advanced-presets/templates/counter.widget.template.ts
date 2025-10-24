dce02aaa95ed7afa4b72b0b1bc1a910553d3515b
Eaglenait <>
2025-10-24 15:47:58 +0200

    Various counter fixes

--- src/app/advanced-presets/templates/counter.widget.template.ts
+++ src/app/advanced-presets/templates/counter.widget.template.ts
@@ -1,17 +1,16 @@
 export class {{CLASS_NAME}} {
-  public containerWidget: mod.UIWidget = null;
-  public textWidget: mod.UIWidget = null;
+  public containerWidget: mod.UIWidget | undefined;
+  public textWidget: mod.UIWidget | undefined;
 
   private counterValue = 0;
 
-  public create(): mod.UIWidget {
+  public create(owner: mod.Player | mod.Team | null = null): void {
     this.counterValue = 0;
     this.containerWidget = modlib.ParseUI(
 {{UI_PARAMS}}
     );
     this.textWidget = mod.FindUIWidgetWithName('CounterText');
     this.refreshUi();
-    return this.containerWidget;
   }
 
   public increment(): number {
@@ -26,13 +25,14 @@ export class {{CLASS_NAME}} {
     return this.counterValue;
   }
 
-  pulic destroy() {
+  public destroy() {
     if (this.containerWidget) {
         mod.DeleteUIWidget(this.containerWidget);
     }
   }
 
   private refreshUi(): void {
+    if (!this.textWidget) return;
     mod.SetUITextLabel(this.textWidget, mod.Message(this.counterValue.toString()));
   }
 
